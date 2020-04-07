d231f91154cbd23709ecf55055d3619f1a0cb339
bobeszhu <bob@bobnet.network>
2020-04-07 20:27:09 +0200

    article editor

--- src/main/kotlin/network/bobnet/cms/controller/backoffice/BackOfficeHtmlController.kt
+++ src/main/kotlin/network/bobnet/cms/controller/backoffice/BackOfficeHtmlController.kt
@@ -1,13 +1,21 @@
 package network.bobnet.cms.controller.backoffice
 
 import network.bobnet.cms.controller.SiteInfoController
+import network.bobnet.cms.model.content.Article
+import network.bobnet.cms.model.user.User
+import network.bobnet.cms.repository.content.ArticleRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.server.ResponseStatusException
+import java.time.LocalDateTime
 
 @Controller
-class BackOfficeHtmlController (private val siteInfoController: SiteInfoController){
+class BackOfficeHtmlController (private val repository: ArticleRepository,
+                                private val siteInfoController: SiteInfoController){
 
     @GetMapping("/admin")
     fun admin(model: Model): String {
@@ -21,8 +29,31 @@ class BackOfficeHtmlController (private val siteInfoController: SiteInfoControll
         return "edit"
     }
 
-    @GetMapping("/admin/profile")
+    @GetMapping("/admin/articles")
     fun edit(model: Model): String{
+        model.addAttribute(siteInfoController.getSiteInfo(model))
+        model["title"] = "Articles"
+        model["articles"] = repository.findAllByOrderByAddedAtDesc().map { it.render() }
+        return "backoffice/articles"
+    }
+
+    @GetMapping("/admin/articles/{slug}")
+    fun article(@PathVariable slug: String, model: Model): String{
+        model.addAttribute(siteInfoController.getSiteInfo(model))
+        val article = repository
+                .findBySlug(slug)
+                ?.render()
+                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
+        model["title"] = "Edit Article"
+        model["articletitle"] = article.title
+        model["articlecontent"] = article.content
+        model["author"] = article.author
+        model["featuredimage"] = article.featuredImage
+        return "backoffice/article"
+    }
+
+    @GetMapping("/admin/profile")
+    fun profile(model: Model): String{
 
         return "profile"
     }
@@ -38,4 +69,28 @@ class BackOfficeHtmlController (private val siteInfoController: SiteInfoControll
 
         return "edituser"
     }
+
+    fun Article.render() = id?.let {
+        RenderedArticle(
+                slug,
+                title,
+                headline,
+                content,
+                featuredImage,
+                author,
+                addedAt,
+                it
+
+        )
+    }
+
+    data class RenderedArticle(
+            val slug: String,
+            val title: String,
+            val headline: String,
+            val content: String,
+            val featuredImage: String,
+            val author: User,
+            val addedAt: LocalDateTime,
+            val id: Long)
 }
