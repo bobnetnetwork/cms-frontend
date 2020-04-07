fc10d65366e86b60877bc7fc9c05bfbea872ce95
bobeszhu <bob@bobnet.network>
2020-04-07 04:51:10 +0200

    site info

---
+++ src/main/kotlin/network/bobnet/cms/controller/OptionsController.kt
@@ -0,0 +1,21 @@
+package network.bobnet.cms.controller
+
+import network.bobnet.cms.repository.OptionsRepository
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
+@RestController
+@RequestMapping("/api/options")
+class OptionsController(private val repository: OptionsRepository) {
+
+    @GetMapping("/")
+    fun findAll() = repository.findAll()
+
+    @GetMapping("/{name}")
+    fun findOne(@PathVariable name: String) =
+            repository.findByName(name) ?: ResponseStatusException(HttpStatus.NOT_FOUND, "This options does not exist")
+}
--- src/main/kotlin/network/bobnet/cms/repository/ArticleRepository.kt
+++ src/main/kotlin/network/bobnet/cms/repository/ArticleRepository.kt
@@ -1,12 +1,11 @@
-package network.bobnet.cms
+package network.bobnet.cms.repository
 
+import network.bobnet.cms.model.Article
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ArticleRepository : CrudRepository<Article, Long> {
     fun findBySlug(slug: String): Article?
     fun findAllByOrderByAddedAtDesc(): Iterable<Article>
-}
-
-interface UserRepository : CrudRepository<User, Long> {
-    fun findByLogin(login: String): User?
 }
