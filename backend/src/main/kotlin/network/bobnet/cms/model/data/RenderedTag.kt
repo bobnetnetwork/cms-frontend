af95099d90445f9e6113a08523ddd23479369ee8
bobesz <bob@bobnet.network>
2020-07-20 01:26:32 +0200

    remove old backend

--- backend/src/main/kotlin/network/bobnet/cms/model/data/RenderedTag.kt
+++
@@ -1,9 +0,0 @@
-package network.bobnet.cms.model.data
-
-import java.time.LocalDateTime
-
-data class RenderedTag(
-        val slug: String,
-        val title: String,
-        val addedAt: LocalDateTime,
-        val id: Long)
