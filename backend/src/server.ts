933b55da97db2a4666a7220690fa46bfd6053ed9
bobesz <bob@bobnet.network>
2020-07-27 18:00:40 +0200

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,8 @@ import * as server from './service/serverService';
 import express from 'express';
 
 const app = express();
+const router = express.Router();
+
+app.use('/api/v01', router);
 
 server.startServer(app);
