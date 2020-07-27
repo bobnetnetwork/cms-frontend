d75f242080fd91db23e2779104d6f1115c0c83fb
bobesz <bob@bobnet.network>
2020-07-27 17:07:02 +0200

    created dbService

---
+++ backend/src/config/config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    'dbServerType': 'MNG',
+    'dbServerAddress': '127.0.0.1',
+    'dbServerPort': '27017',
+    'dbServerUser': 'cms_dev',
+    'dbServerPwd': 'Almafa1.',
+    'dbServerDataBase': 'cms_dev',
+
+};
