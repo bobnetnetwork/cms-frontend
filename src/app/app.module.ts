fda83175ff420bed09c84af19edd098a3a64c2c9
bobesz <bob@bobnet.network>
2020-09-06 11:40:19 +0200

    moderna templaet added to frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './components/template/appheader/appheader.component';
+import { AppfooterComponent } from './components/template/appfooter/appfooter.component';
+import { AppmenuComponent } from './components/template/appmenu/appmenu.component';
+import { AppsidebarComponent } from './components/template/appsidebar/appsidebar.component';
+import { AppcontentComponent } from './components/template/appcontent/appcontent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppfooterComponent,
+    AppmenuComponent,
+    AppsidebarComponent,
+    AppcontentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/template/appsidebar/appsidebar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppsidebarComponent } from './appsidebar.component';
+
+describe('AppsidebarComponent', () => {
+  let component: AppsidebarComponent;
+  let fixture: ComponentFixture<AppsidebarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppsidebarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppsidebarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
